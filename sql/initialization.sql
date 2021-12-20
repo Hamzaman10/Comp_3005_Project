5a1c90d9ea1284e8e22232bf7faf02aa4f4bca7b
monishkumar-sivakumar <63323924+monishkumar-sivakumar@users.noreply.github.com>
2021-12-19 21:27:29 -0500

    Update initialization.sql

--- sql/initialization.sql
+++ sql/initialization.sql
@@ -1,4 +1,4 @@
-// the next 5 lines are inserting default values into book
+/* the next 5 lines are inserting default values into book */
 
 insert into "book" (ISBN, book_name, price, number_of_pages, quantity, royalities, genre)
 values('15151', 'Dune: Deluxe Edition', 32.22, 688, 10, 5, 'Sci-Fi');
@@ -15,7 +15,7 @@ values('86412', 'Darkwing', 16.56, 254, 15, 6, 'Mystery');
 insert into "book" (ISBN, book_name, price, number_of_pages, quantity, royalities, genre)
 values('14528', 'Firewing', 14.56, 278, 15, 4.6, 'Mystery');
 
-// the next 5 lines are inserting default values into warehouse
+/* the next 5 lines are inserting default values into warehouse */
 
 insert into "warehouse" (warehouse_id, "name", phone_number, "location", ISBN)
 values( 1, 'torontohouse', 6478921414, 'toronto', '15151');
@@ -32,7 +32,7 @@ values(4, 'torontohouse', 6478921414, 'toronto', '86412');
 insert into "warehouse" (warehouse_id, "name", phone_number, "location", ISBN)
 values(5, 'torontohouse', 6478921414, 'toronto', '14528');
 
-// the next 3 lines are inserting default values into account_details
+/* the next 3 lines are inserting default values into account_details */
 
 insert into "account_details" (account_name, "password", account_type, first_name, last_name, email, phone_number)
 values( 'admin', 'admin123', 'admin', 'John', 'Smith', 'john@gmail.com', 4169982134);
@@ -43,7 +43,7 @@ values( 'cale', 'cale123', 'regular', 'Cale', 'White', 'cale@gmail.com', 4169921
 insert into "account_details" (account_name, "password", account_type, first_name, last_name, email, phone_number)
 values( 'matt', 'matt123', 'regular', 'Matt', 'Patt', 'matt@gmail.com', 4165149990);
 
-// the next 8 lines are inserting default values into address
+/* the next 8 lines are inserting default values into address */
 
 insert into "address" (address_id, address_type, post_code, street_name, street_number, province)
 values(1, 'shipping', 'M2X1G7', 'Dort dr.', '46', 'Ontario');
@@ -69,6 +69,8 @@ values( 7, 'billing', 'B2T8J9', 'Berry dr.', '164', 'Ontario');
 insert into "address" (address_id,address_type, post_code, street_name, street_number, province)
 values( 8, 'billing', 'B1Z7U9', 'Vine st.', '15', 'Ontario');
 
+/* the next 7 lines are inserting default values into banking_info */
+
 insert into "banking_info" (card_number, exp_date, CVV_number)
 values( 14156572, '2022-11-23', '123');
 
@@ -90,6 +92,8 @@ values( 67533128, '2016-02-01', '542');
 insert into "banking_info" (card_number, exp_date, CVV_number)
 values( 12345123, '2021-07-15', '175');
 
+/* the next 5 lines are inserting default values into account_main */
+
 insert into "account_main" (account_id, account_name, address_id, card_number)
 values(1,'admin', 1, 20003444);
 
@@ -105,6 +109,8 @@ values(4,'matt', 5, 56743125);
 insert into "account_main" (account_id,account_name, address_id, card_number)
 values(5,'matt', 6, 56743128);
 
+/* the next 3 lines are inserting default values into publisher_details */
+
 insert into "publisher_details" (publisher_name, email, phone_number)
 values('Scholastic', 'scholastic@gmail.com', 6479112032);
 
@@ -113,6 +119,8 @@ values('BooksRUS', 'booksrus@gmail.com', 6471441213);
 
 insert into "publisher_details" (publisher_name, email, phone_number)
 values('BooksFan', 'booksfan@gmail.com', 4125461234);
+
+/* the next 5 lines are inserting default values into publisher_books */
 
 insert into "publisher_books" (publisher_id, publisher_name, address_id, card_number, ISBN)
 values(1, 'Scholastic', 2, 14156572, '15151');
@@ -129,6 +137,8 @@ values(4, 'BooksRUS', 7, 67533128, '14528');
 insert into "publisher_books" (publisher_id, publisher_name, address_id, card_number, ISBN)
 values(5, 'BooksFan', 8, 12345123, '31342');
 
+/* the next 4 lines are inserting default values into author */
+
 insert into "author" (author_id, email, first_name, last_name, phone_number)
 values(1, 'rick@gmail.com', 'Rick', 'Riordan', 6123519090);
 
@@ -140,6 +150,8 @@ values(3, 'frank@gmail.com', 'Frank', 'Herbert', 4160508667);
 
 insert into "author" (author_id, email, first_name, last_name, phone_number)
 values(4, 'kenneth@gmail.com', 'Kenneth', 'Oppel', 4160508667);
+
+/* the next 5 lines are inserting default values into written_by */
 
 insert into "written_by" (author_id, ISBN)
 values(2, '23232');
@@ -156,14 +168,20 @@ values(4, '14528');
 insert into "written_by" (author_id, ISBN)
 values(4, '86412');
 
+/* the next 2 lines are inserting default values into basket_main */
+
 insert into "basket_amount" (basket_owner, price, amount_added)
 values((select account_name from account_main where account_id = 2), 32, 1);
 
 insert into "basket_main" (account_basket, basket_owner, account_id, ISBN)
 values( 1, (select account_name from account_main where account_id = 2), 2, '15151' );
 
+/* the next line is inserting default values into orders */
+
 insert into "orders" (order_id, address_id)
 values('1', 3);
+
+/* the next 2 lines are updating basket_amount and basket_main to now have an order showing on it */
 
 UPDATE basket_main
 SET order_id = '1'
@@ -172,6 +190,8 @@ WHERE order_id IS NULL and account_basket = 1;
 UPDATE basket_amount
 set amount_added = amount_added + 1
 where basket_owner = 'cale';
+
+/* the next 2 lines are inserting default values into tracking_details and tracking_main*/
 
 insert into "tracking_details" (tracking_id,shipping_status,"location")
 values(1,'On Route', 'Toronto' );
