a5dd091949c0cdd43b78ffbb0af67cef2998daeb
monishkumar-sivakumar <63323924+monishkumar-sivakumar@users.noreply.github.com>
2021-12-19 21:31:41 -0500

    Update triggers.sql

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -1,11 +1,26 @@
-(inside trigger)
+/*trigger based on lectures*/
+create trigger restock after update of quantity on book
+	referencing new row as nrow
+	referencing old row as orow
+	for each row
+	when nrow.quantity < 10 
+	and orow.quantity >= 10
+	begin atomic
+		update book 
+		set quantity = 15
+		where quantity < 10
+	end; 
+
+/* I used the create trigger and trigger function of pgadmin since the code above wasn't working */
+
+/*inside trigger, created using the pgadmin create button*/
 CREATE TRIGGER restock
     AFTER UPDATE OF quantity
     ON public.book
     FOR EACH ROW
     EXECUTE FUNCTION public.refill();
 
-(inside trigger function )
+/*inside trigger function, created using the pgadmin create button */
 BEGIN
 	update book 
 	set quantity = 15
